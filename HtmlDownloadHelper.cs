ad2beb5ce33c390b8664148d775763e05e8902e8
ultv <ultv@inbox.ru>
2018-09-16 10:33:29 +0400

    create HtmlDownloadHelper

---
+++ HtmlDownloadHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace ParserHTML
+{
+    public class HtmlDownloadHelper
+    {
+        public static string DownloadHtml(string uri)
+        {
+            HttpWebRequest request = WebRequest.Create(uri) as HttpWebRequest;
+            HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+
+            StreamReader sr = new StreamReader(response.GetResponseStream());
+            string html = sr.ReadToEnd();
+
+            return "";
+        }
+    }
+}
