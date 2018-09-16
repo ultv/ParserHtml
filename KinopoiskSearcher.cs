0ac21afc38c2452524977be8295878635d78fe47
ultv <ultv@inbox.ru>
2018-09-16 10:19:28 +0400

    create KinopoiskSearcher

---
+++ KinopoiskSearcher.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParserHTML
+{
+    public class KinopoiskSearcher
+    {
+        private string _html;
+        private Image _cover;
+        private readonly string BaseUrl;
+
+        public KinopoiskSearcher()
+        {
+            BaseUrl = @"https://www.kinopoisk.ru/index.php?first=yes&kp_query={0}";
+        }
+
+        public bool DownloadHTML(string moveName)
+        {
+            return false;
+        }
+
+        public bool FindCover()
+        {
+            return false;
+        }
+    }
+}
